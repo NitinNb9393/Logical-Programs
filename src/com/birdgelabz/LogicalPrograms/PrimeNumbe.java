1168813c62e14bd48f9069971e1b0f5115b53313
NitinNb9393 <nitinbabar9393@gmail.com>
2021-10-25 14:21:54 +0530

    Added:Prime or not prime

---
+++ src/com/birdgelabz/LogicalPrograms/PrimeNumbe.java
@@ -0,0 +1,27 @@
+package com.birdgelabz.LogicalPrograms;
+
+import java.util.Scanner;
+
+public class PrimeNumbe {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a Number .");
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		sc.close();
+		int counter = 0;
+		for (int i = 2; i <= number / 2; i++) {
+
+			if (number % i == 0) {
+				counter = 1;
+				;
+				break;
+			}
+		}
+
+		if (counter == 0)
+			System.out.println(number + " is a prime number.");
+		else
+			System.out.println(number + " is not a prime number.");
+	}
+}
